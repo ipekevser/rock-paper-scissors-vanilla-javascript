b5a36bfb9785c0173c461589dd9a35d98513047f
ipekevser <kvsr.ipk@gmail.com>
2022-04-10 18:53:27 +0300

    add winning score feature

--- script.js
+++ script.js
@@ -1,46 +1,77 @@
 const selectButtons = document.querySelectorAll("#selectButtons");
-const icons = ["✌️","✋","✊"];
+const icons = ["✌️", "✋", "✊"];
 const playConsole = ["Rock", "Paper", "Scissors"];
 const playerTable = document.querySelector("#playerScore");
 const computerTable = document.querySelector("#computerScore");
 const resetButton = document.querySelector("#resetButton");
-let playerScore = 0;
-let computerScore = 0;
-const computer = document.querySelector("#computer");
-const player = document.querySelector("#player");
-let playerChoice;
+const computerText = document.querySelector("#computer");
+const playerText = document.querySelector("#player");
 const computerCard = document.getElementById("computerCard");
 const playerCard = document.getElementById("playerCard");
 const buttons = document.querySelectorAll("button");
-
+const winningScoreSelector = document.querySelector("#playto");
+let playerScore = 0;
+let computerScore = 0;
+let winningScore = 3;
+let playerChoice;
 
 let newDiv = document.createElement("div");
-newDiv.classList.add = "text-uppercase";
+newDiv.classList.add("text-uppercase", "fw-bold", "mt-3", "p-2");
 document.body.appendChild(newDiv);
 
 let playerDiv = document.createElement("div");
-playerDiv.classList.add = "card-icon";
+playerDiv.classList.add("card-icon");
 playerCard.appendChild(playerDiv);
 
 let computerDiv = document.createElement("div");
-computerDiv.classList.add = "card-icon";
+computerDiv.classList.add("card-icon");
 computerCard.appendChild(computerDiv);
 
 function playerFunction(player, computer) {
-  playerScore++;
-  playerTable.innerHTML = playerScore;
-  newDiv.innerHTML = `${player} Wins! ${computer} lost!`;
+  if (playerScore != winningScoreSelector.value) {
+    playerScore++;
+    if (playerScore == winningScoreSelector.value) {
+      selectButtons.forEach((element) => (element.disabled = true));
+      playerText.classList.add("text-success");
+      computerText.classList.add("text-danger");
+    }
+    playerTable.innerHTML = playerScore;
+    newDiv.innerHTML = `${player} Wins! ${computer} lost!`;
+  }
 }
 function computerFunction(player, computer) {
-  computerScore++;
+  if (computerScore != winningScoreSelector.value) {
+    computerScore++;
+    if (computerScore == winningScoreSelector.value) {
+      selectButtons.forEach((element) => (element.disabled = true));
+      computerText.classList.add("text-success");
+      playerText.classList.add("text-danger");
+    }
+    computerTable.innerHTML = computerScore;
+    newDiv.innerHTML = `${computer} Wins! ${player} lost!`;
+  }
+}
+function resetFunction() {
+  playerDiv.innerHTML = "";
+  computerDiv.innerHTML = "";
+  playerScore = 0;
+  playerTable.innerHTML = playerScore;
+  computerScore = 0;
   computerTable.innerHTML = computerScore;
-  newDiv.innerHTML = `${computer} Wins! ${player} lost!`;
+  newDiv.innerHTML = "";
+  selectButtons.forEach((element) => (element.disabled = false));
+  playerText.classList.remove("text-danger", "text-success");
+  computerText.classList.remove("text-danger", "text-success");
 }
 
+winningScoreSelector.addEventListener("change", function () {
+  winningScore = parseInt(winningScoreSelector.value);
+  resetFunction();
+  return winningScore;
+});
 
 for (let button of buttons) {
   button.addEventListener("click", function () {
-    console.log(button.innerHTML);
     playerChoice = button.innerHTML;
     let computerChoice =
       playConsole[Math.floor(Math.random() * playConsole.length)];
@@ -49,18 +80,15 @@ for (let button of buttons) {
       if (computerChoice === "Scissors") {
         computerDiv.innerHTML = icons[0];
         playerFunction(playerChoice, computerChoice);
-        
       } else if (computerChoice === "Paper") {
         computerDiv.innerHTML = icons[1];
         computerFunction(playerChoice, computerChoice);
-       
       } else {
         computerDiv.innerHTML = icons[2];
         newDiv.innerHTML = "Equal. Both of them are rock!";
-        
       }
     } else if (playerChoice === "Paper") {
-      playerDiv.innerHTML= icons[1];
+      playerDiv.innerHTML = icons[1];
       if (computerChoice === "Rock") {
         playerFunction(playerChoice, computerChoice);
         computerDiv.innerHTML = icons[2];
@@ -71,8 +99,8 @@ for (let button of buttons) {
         newDiv.innerHTML = "Equal. Both of them are paper!";
         computerDiv.innerHTML = icons[1];
       }
-    }else if (playerChoice === "Scissors") {
-      playerDiv.innerHTML= icons[0];
+    } else if (playerChoice === "Scissors") {
+      playerDiv.innerHTML = icons[0];
       if (computerChoice === "Rock") {
         computerFunction(playerChoice, computerChoice);
         computerDiv.innerHTML = icons[2];
@@ -84,13 +112,7 @@ for (let button of buttons) {
         computerDiv.innerHTML = icons[0];
       }
     } else {
-      playerDiv.innerHTML = "";
-      computerDiv.innerHTML = "";
-      playerScore = 0;
-      playerTable.innerHTML = playerScore;
-      computerScore = 0;
-      computerTable.innerHTML = computerScore;
-      newDiv.innerHTML = "";
+      resetFunction();
     }
   });
 }
